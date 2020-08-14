81184047b37996da870ab8bb93b973c670f9ff93
baurine <2008.hbl@gmail.com>
2020-08-14 17:07:33 +0800

    init router

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -3,99 +3,55 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Link,
-  Outlet,
-  useParams,
+  NavLink,
 } from 'react-router-dom'
+import { Layout, Menu } from 'antd'
+import {
+  HddOutlined,
+  DeploymentUnitOutlined,
+  ClusterOutlined,
+} from '@ant-design/icons'
 import './App.less'
 
-function Home() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h2>Home View</h2>
-      <p>在 React 中使用 React Router v6 的指南</p>
-    </div>
-  )
+const { Sider, Content } = Layout
+
+function Machines() {
+  return <div>Machines</div>
 }
 
-function About() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h2>About View</h2>
-      <p>在 React 中使用 React Router v6 的指南</p>
-    </div>
-  )
+function Deployment() {
+  return <div>Deployment</div>
 }
 
-const BlogPosts = {
-  '1': {
-    title: 'First Blog Post',
-    description: 'Lorem ipsum dolor sit amet, consectetur adip.',
-  },
-  '2': {
-    title: 'Second Blog Post',
-    description: 'Hello React Router v6',
-  },
-}
-
-function Posts() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h2>Blog</h2>
-      {/* 渲染任何匹配的子级 */}
-      <Outlet />
-    </div>
-  )
-}
-
-function PostLists() {
-  return (
-    <ul>
-      {Object.entries(BlogPosts).map(([slug, { title }]) => (
-        <li key={slug}>
-          <Link to={`/posts/${slug}`}>
-            <h3>{title}</h3>
-          </Link>
-        </li>
-      ))}
-    </ul>
-  )
-}
-
-function Post() {
-  const { slug } = useParams()
-  const { title, description } = (BlogPosts as any)[slug]
-
-  return (
-    <div style={{ padding: 20 }}>
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </div>
-  )
+function Clusters() {
+  return <div>Clusters</div>
 }
 
 function App() {
   return (
     <Router>
-      <nav style={{ margin: 10 }}>
-        <Link to="/" style={{ padding: 5 }}>
-          Home
-        </Link>
-        <Link to="/about" style={{ padding: 5 }}>
-          About
-        </Link>
-        <Link to="/posts" style={{ padding: 5 }}>
-          Posts
-        </Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/posts" element={<Posts />}>
-          <Route path="/" element={<PostLists />} />
-          <Route path="/:slug" element={<Post />} />
-        </Route>
-      </Routes>
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider collapsible>
+          <Menu theme="dark" defaultSelectedKeys={['/']} mode="inline">
+            <Menu.Item key="/" icon={<HddOutlined />}>
+              <NavLink to="/">配置机器</NavLink>
+            </Menu.Item>
+            <Menu.Item key="/deploy" icon={<DeploymentUnitOutlined />}>
+              <NavLink to="/deploy">部署</NavLink>
+            </Menu.Item>
+            <Menu.Item key="/clusters" icon={<ClusterOutlined />}>
+              <NavLink to="/clusters">集群管理</NavLink>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Content style={{ backgroundColor: 'white', padding: '16px' }}>
+          <Routes>
+            <Route path="/" element={<Machines />} />
+            <Route path="/deploy" element={<Deployment />} />
+            <Route path="/clusters" element={<Clusters />} />
+          </Routes>
+        </Content>
+      </Layout>
     </Router>
   )
 }
