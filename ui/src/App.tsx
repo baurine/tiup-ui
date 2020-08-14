903d8535f21732aa9e024e07ff86d6a296083580
baurine <2008.hbl@gmail.com>
2020-08-14 16:06:30 +0800

    use react-router 6

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,10 +1,103 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Outlet,
+  useParams,
+} from 'react-router-dom'
 import './App.less'
 
-import { Button } from 'antd'
+function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>Home View</h2>
+      <p>在 React 中使用 React Router v6 的指南</p>
+    </div>
+  )
+}
+
+function About() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>About View</h2>
+      <p>在 React 中使用 React Router v6 的指南</p>
+    </div>
+  )
+}
+
+const BlogPosts = {
+  '1': {
+    title: 'First Blog Post',
+    description: 'Lorem ipsum dolor sit amet, consectetur adip.',
+  },
+  '2': {
+    title: 'Second Blog Post',
+    description: 'Hello React Router v6',
+  },
+}
+
+function Posts() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>Blog</h2>
+      {/* 渲染任何匹配的子级 */}
+      <Outlet />
+    </div>
+  )
+}
+
+function PostLists() {
+  return (
+    <ul>
+      {Object.entries(BlogPosts).map(([slug, { title }]) => (
+        <li key={slug}>
+          <Link to={`/posts/${slug}`}>
+            <h3>{title}</h3>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+function Post() {
+  const { slug } = useParams()
+  const { title, description } = (BlogPosts as any)[slug]
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  )
+}
 
 function App() {
-  return <Button type="primary">TiUP</Button>
+  return (
+    <Router>
+      <nav style={{ margin: 10 }}>
+        <Link to="/" style={{ padding: 5 }}>
+          Home
+        </Link>
+        <Link to="/about" style={{ padding: 5 }}>
+          About
+        </Link>
+        <Link to="/posts" style={{ padding: 5 }}>
+          Posts
+        </Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/posts" element={<Posts />}>
+          <Route path="/" element={<PostLists />} />
+          <Route path="/:slug" element={<Post />} />
+        </Route>
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
